d26ed46065e0bacdf8b2e22bc04900c107d5a07e
Bassel <basel_ebeed1@yahoo.com>
2022-03-12 05:44:16 +0200

    dicts

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, flash
 from joblib import load
 from dicts import outputs, country_codes
-import  Data_pre_processing
+import  dicts
 
 
-pipeline = load("tweet_dialect.joblib")
+pipeline = load("two_gram_svm.joblib")
 
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
@@ -16,7 +16,7 @@ def index():
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
-	pocessed_text = Data_pre_processing.preprocess(str(request.form['name_input']))
+	pocessed_text = dicts.preprocess(str(request.form['name_input']))
 	temp = pipeline.predict([pocessed_text])
 	flash(country_codes[outputs[temp[0]]])  
 	return render_template("index.html")
--- dicts.py
+++ dicts.py
@@ -1,9 +1,74 @@
+import itertools
 import pandas as pd
+import json
+import requests
+import re
+from farasa.stemmer import FarasaStemmer
+import pyarabic.araby as araby
+from pyarabic.araby import tokenize, is_arabicrange, strip_tashkeel
+
+# Removing mentions
+def remove_mentions(text):
+    return re.sub("@[A-Za-z0-9_]+","", text)
+
+# Removing links
+def remove_links(text):
+    return re.sub(r"http\S+", "", text)
+    return(text)
+
+# Normalizing Arabic letters
+def normalizeArabic(text):
+    text = text.replace("أ", "ا" )
+    text = text.replace("إ", "ا")
+    text = text.replace("آ", "ا" )
+    text = text.replace("ى", "ي")
+    text = text.replace("ة","ه")
+    text = text.replace('اً', 'ا')
+    return(text)
+
+# Removing hashtags
+def remove_hashtags(text):
+    #return " ".join(filter(lambda text:text[0]!='#', text.split()))
+    st = '# _'
+    for i, letter in enumerate(text):
+        if letter in st:
+            text = text.replace(letter," ")
+    return text
+
+# Remove El-thshkeel and emoji, numbers and punctuations
+def remove_el_tashkeel_and_other_non_letters(text):
+    text = tokenize(text, conditions=is_arabicrange, morphs=strip_tashkeel)
+    
+    return ' '.join(text)
+
+# Remove repeated letters
+def remove_repeated_letters(text):
+    # words like ههههههه  will be ه
+    # words like جدااااا will be جدا
+    return ''.join(c for c, _ in itertools.groupby(text))
+
+# Lemmatization
+stemmer_interactive = FarasaStemmer(interactive=True)
+def lemmatization(text):
+    stemmed_interactive = stemmer_interactive.stem(text)
+    return stemmed_interactive
+
+# proccess
+def preprocess(text):
+    text = remove_mentions(text)
+    text = remove_links(text)
+    text = normalizeArabic(text)
+    text = remove_hashtags(text)
+    text = remove_el_tashkeel_and_other_non_letters(text)
+    text = remove_repeated_letters(text)
+    text = lemmatization(text)
+    return text
 
 dataset = pd.read_csv('preprocessed_data.csv')
 
 dataset['dialect_number'] = dataset['dialect'].factorize()[0]
 outputs = dict(zip(dataset['dialect_number'], dataset['dialect']))
+
 country_codes = {'IQ':'Iraq',
 'BH':'Bahrain',
 'KW':'Kuwait',
